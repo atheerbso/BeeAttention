c2f6ea1742fcfe1e986e2927e92efa11ad072e5f
atheer <atheerbso@gmail.com>
2024-01-23 11:39:41 +0300

    Initial Commit

---
+++ BeeAttention/BeeAttentionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeAttentionApp.swift
+//  BeeAttention
+//
+//  Created by Atheer on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct BeeAttentionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeeAttention/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeeAttention
+//
+//  Created by Atheer on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
