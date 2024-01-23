975735be4f26e640f0281515a86eb55e71b2f2ce
Shahad842 <shahad_9215@hotmail.com>
2024-01-23 12:10:55 +0300

    -

---
+++ BeeAttention/SwiftUIView1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView1.swift
+//  BeeAttention
+//
+//  Created by Shahad Alhothali on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView1()
+}
